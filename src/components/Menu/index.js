2f84cc622323fe366dea3125c6575eede871ac01
Rudolf <rodolfo.okino@gmail.com>
2020-07-29 21:22:25 +0000

    Setup inicial do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../../assets/img/logo-flix.png'
+import './Menu.css'
+// import ButtonLink from '../components/ButtonLink'
+import Button from '../Button'
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="Logo da DoolpFlix" />
+            </a>
+            <Button as='a' className="ButtonLink">
+                Novo vídeo
+            </Button>
+        </nav>
+    )
+}
+
+export default Menu
